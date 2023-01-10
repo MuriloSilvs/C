865bfd2b6086777f8dcc7cb589fd7b968310d546
Murilo Oliveira Silva <101263628+MuriloSilvs@users.noreply.github.com>
2023-01-09 21:59:15 -0300

    Entrada de dados
    Aprendi a utilizar a função scanf que permite a entrada de dados pelo teclado

---
+++ 05 - Scanf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main (){
+	
+    int var;
+	
+	printf("Insira um valor para a variavel: ");
+	scanf("%d", &var); //Permiti que o usuário faça a entrada do dado
+	
+	printf("\nvalor escolhido: %d", var); //Imprimi o valor inserido pelo usuário
+	
+	return 0;
+}
