3068116c2921106932d732862faeead678a3fd2d
Murilo Oliveira Silva <101263628+MuriloSilvs@users.noreply.github.com>
2023-01-09 21:52:06 -0300

    Add files via upload

---
+++ 05 - Scanf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main (){
+	
+    int var;
+	
+	printf("\n Insira um valor para a variavel: ");
+	scanf("%d", &var1); //Permiti que o usuário faça a entrada do dado
+	
+	printf("\n valor escolhido: %d.", var); //Imprimi os valo inserido pelo usuário
+	
+	return 0;
+}
